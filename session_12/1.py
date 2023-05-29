4467d8bc9251b61b0bd9dc60a536696ea8814f11
dimasic <litrox@mail.ru>
2023-05-29 14:53:05 +0700

    Решена задача вхождения подстроки.

--- session_12/1.py
+++ session_12/1.py
@@ -1,12 +1,25 @@
 def get_occurrences_count(string, desired_string):
     lower_case_string = string.lower()
+    occurrences_count = 0
+    current_index = 0
+    lower_case_string.find(desired_string, current_index)
 
-    if lower_case_string.find(desired_string):
-        pass
+    while True:
+        if lower_case_string.find(desired_string, current_index) == -1:
+            break
+
+        occurrences_count += 1
+        current_index = lower_case_string.find(desired_string, current_index) + len(desired_string)
+
+    return occurrences_count
 
 
-occurrences_count = 0
+#print(get_occurrences_count('лес лес лес поле лес лес ЛЕС', 'лес'))
 
-with open('input.txt', 'r', encoding='utf-8') as input_file:
-    for line in input_file:
-        pass
+count = 0
+
+with open('input.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        count += get_occurrences_count(line, 'лес')
+
+print(count)
