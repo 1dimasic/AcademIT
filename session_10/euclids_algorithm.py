f5f7e856e0a2f1381fe0e3a63ba7c50673a03668
dimasic <litrox@mail.ru>
2023-06-01 14:50:32 +0700

    Корректировка к уроку 10

--- session_10/euclids_algorithm.py
+++ session_10/euclids_algorithm.py
@@ -1,15 +1,15 @@
-def get_greatest_common_divisor(_number_1, _number_2):
-    if _number_2 == 0:
-        return _number_1
+def get_greatest_common_divisor(number_1, number_2):
+    if number_2 == 0:
+        return number_1
 
-    return get_greatest_common_divisor(_number_2, _number_1 % _number_2)
+    return get_greatest_common_divisor(number_2, number_1 % number_2)
 
 
-number_1 = int(input('Введите первое число: '))
-number_2 = int(input('Введите второе число: '))
+integer_1 = int(input('Введите первое число: '))
+integer_2 = int(input('Введите второе число: '))
 
-if number_1 == 0 and number_2 == 0:
+if integer_1 == 0 and integer_2 == 0:
     print('Наибольший общий делитель найти нельзя')
 else:
-    greatest_common_divisor = get_greatest_common_divisor(number_1, number_2)
+    greatest_common_divisor = get_greatest_common_divisor(integer_1, integer_2)
     print(f'Наибольший общий делитель = {greatest_common_divisor}')
