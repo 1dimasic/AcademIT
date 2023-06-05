1c4f26efc44f57466c8b93e68bc2cfab44885018
dimasic <litrox@mail.ru>
2023-06-05 16:16:00 +0700

    добавлен решение поиска определителя матрицы

--- session_14/1.py
+++ session_14/1.py
@@ -1,22 +1,16 @@
-import numpy as np
-
-#
-# def g(array, j):
-#     l = array[1:, j:] + array[1:, :j + 1]
-#     return l
-#
-#
-# def f(array):
-#     if len(array) == 2:
-#         return array[0, 0] * array[1, 1] - array[0, 1] * array[1, 0]
-#
-#     d = 0
-#     for i in range(len(l)):
-#         d += (-1) ** i * array[0][i] * f(g(array, i))
-#     return d
+def g(matrix, i):
+    return [row[:i] + row[i + 1:] for row in matrix[1:]]
 
 
-# l = np.array([[1, 2, 1], [2, 1, 2], [1, 1, 2]])
-l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-for i in range(len(l)):
-    print(l[1:][i:])
+def f(matrix):
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    d = 0
+    for i in range(len(matrix)):
+        d += (-1) ** i * matrix[0][i] * f(g(matrix, i))
+    return d
+
+
+D = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
