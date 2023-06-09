1234556e28b7cd9b80be3412e8321c0a2e1b3e14
dimasic <litrox@mail.ru>
2023-06-09 15:24:43 +0700

    09/06 - 2

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-help(str)
+line = abcbaflhahalghal
+line = line[:2] + line[5:] + line[:]
--- session_14/2.py
+++ session_14/2.py
@@ -1,3 +1,18 @@
+def f(line):
+    c = []
+    b = -1
+
+    while True:
+        b = line.find('"', b + 1)
+        if b != -1:
+            c.append(b)
+            continue
+
+        break
+
+    return c
+
+
 with open('input.csv', 'r', encoding='utf-8') as csv_file, open('output.html', 'w') as html_file:
     html_file.write('<table border=1>\n')
 
@@ -10,33 +25,21 @@ with open('input.csv', 'r', encoding='utf-8') as csv_file, open('output.html', '
         if a % 2 != 0:
             continue
 
-        c = []
-        b = -1
+        d = f(html_line)
 
-        while True:
-            b = html_line.find('"', b + 1)
-            if b != -1:
-                c.append(b)
-                continue
-
-            break
-
-        if c:
-            html_line = html_line[:c[0]] + html_line[c[0] + 1:] + html_line[:c[-1]] + html_line[c[-1] + 1:]
-            print(html_line)
-
-        html_line = ''
-        a = 0
+        if d:
+            html_line = html_line[:d[0]] + html_line[d[0] + 1:d[-1]] + html_line[d[-1] + 1:]
+            f = f(html_line)
 
 
+    # tables_detail = html_line.split(',')
 
-    #     tables_detail = html_line.split(',')
-    #
-    #     for i in range(len(tables_detail)):
-    #         html_file.write(f'<td>{tables_detail[i]}</td>\n')
-    #
+    for i in range(len(tables_detail)):
+        html_file.write(f'<td>{tables_detail[i]}</td>\n')
 
-    #
-    #     html_file.write('</tr>\n')
-    #
-    # html_file.write('</table>')
+        html_file.write('</tr>\n')
+
+    html_file.write('</table>')
+
+    html_line = ''
+    a = 0
