2cbfdcb6f7d142a2a79f51ec2e5ded3e463d1eb3
dimasic <litrox@mail.ru>
2023-09-29 15:29:37 +0700

    add csv_task

--- session_14/csv.py
+++ session_14/csv.py
@@ -1,45 +1,50 @@
-# def f(line):
-#     c = []
-#     b = -1
-#
-#     while True:
-#         b = line.find('"', b + 1)
-#         if b != -1:
-#             c.append(b)
-#             continue
-#
-#         break
-#
-#     return c
-
-
 with open('input.csv', 'r', encoding='utf-8') as csv_file, open('output.html', 'w') as html_file:
-    html_file.write('<table border=1>\n')
+    html_file.write('<table border=1>')
 
-    html_line = ''
-    a = 0
+    input_line = ''
+    quotes_count = 0
+
     for line in csv_file:
-        html_line += line
-        a += line.count('"')
+        input_line += line
+        quotes_count += line.count('"')
 
-        if a % 2 != 0:
+        if quotes_count % 2 != 0:
             continue
 
-        d = f(html_line)
+        if input_line == '\n':
+            input_line = ''
+            quotes_count = 0
+            continue
 
-        if d:
-            html_line = html_line[:d[0]] + html_line[d[0] + 1:d[-1]] + html_line[d[-1] + 1:]
-            f = f(html_line)
+        row = []
+        current_index = 0
 
+        for i in range(len(input_line)):
+            if input_line[i] == ',':
+                if input_line[current_index:i].count('"') % 2 == 0:
+                    row.append(input_line[current_index:i])
+                    current_index = i + 1
 
-    # tables_detail = html_line.split(',')
+        row.append(input_line[current_index:].rstrip())
 
-    for i in range(len(tables_detail)):
-        html_file.write(f'<td>{tables_detail[i]}</td>\n')
+        for i in range(len(row)):
+            if row[i]:
+                if row[i][0] == '"' and row[i][-1] == '"':
+                    row[i] = row[i][1:-1]
 
-        html_file.write('</tr>\n')
+        for i in range(len(row)):
+            row[i] = row[i].replace('""', '"')
+            row[i] = row[i].replace('\n', '<br/>')
+
+        html_file.write('<tr>')
+
+        for i in range(len(row)):
+            html_file.write(f'<td>{row[i]}</td>\t')
+
+        html_file.write('</tr>')
+        html_file.write('<br/>')
+
+        input_line = ''
+        quotes_count = 0
 
     html_file.write('</table>')
-
-    html_line = ''
-    a = 0
